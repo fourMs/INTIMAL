4658fd832924c2e822ec99260e6a93b07a5d5bb6
Paul Boddie <paul.boddie@imv.uio.no>
2019-01-10 18:05:32 +0100

    Permit specification of the synonym language.

--- synonyms.py
+++ synonyms.py
@@ -11,15 +11,15 @@ from nltk.corpus import wordnet as wn
 
 # Mapping via WordNet.
 
-def map_to_synonyms(words):
+def map_to_synonyms(words, lang="spa"):
 
     "Map 'words' to synonyms for normalisation."
 
     l = []
     for word in words:
         s = set()
-        for synset in wn.synsets(word, lang="spa"):
-            for synonym in synset.lemma_names(lang="spa"):
+        for synset in wn.synsets(word, lang=lang):
+            for synonym in synset.lemma_names(lang=lang):
                 s.add(synonym)
         l.append(Term(word, s))
 
