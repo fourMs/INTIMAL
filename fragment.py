4d8b9ebcb7190ad89355abd5b73d069e5b0e5464
Paul Boddie <paul.boddie@imv.uio.no>
2019-01-17 17:16:51 +0100

    Moved output-related functions into a separate module. Introduced a convenience abstraction for the output directory.

--- fragment.py
+++ fragment.py
@@ -5,14 +5,6 @@
 Collect words into the identified fragments, reducing the words to only those
 of importance. Define fragment similarity by identifying common terms,
 potentially weighting some terms as being more significant than others.
-Produce a graph describing the fragments and their relationships.
-
-Using a visualisation tool to show the graph has limited benefit. The
-relationships between pairs of fragments are most important in this application.
-However, a layout algorithm will attempt to reconcile the demands made by each
-relationship simultaneously. Consequently, two fragments that have a high
-similarity may be pulled apart by the relationships such fragments have with
-others.
 """
 
 from inputs import get_input_filenames, \
@@ -31,21 +23,17 @@ from objects import Category, Fragment, \
 from analysis import process_fragment_tokens, \
                      lower_word, stem_word
 
-from graph import write_graph
-
 from grouping import group_words
 
 from stopwords import filter_terms_by_pos
 
-from text import lower, normalise_accents, remove_punctuation
+from text import normalise_accents
 
-from utils import cmp_value_lengths, cmp_values
+import graph
+import outputs
 
 from collections import defaultdict
-from os import mkdir
-from os.path import isdir, join
 from xml.dom.minidom import parse
-import codecs
 import sys
 
 # Fragment processing.
@@ -74,149 +62,6 @@ def get_common_terms(entity_terms):
             d[term].add(entity)
 
     return d
-
-# Output conversion.
-
-def show_all_words(words, filename):
-
-    "Show 'words' in 'filename'."
-
-    out = codecs.open(filename, "w", encoding="utf-8")
-    try:
-        for word in words:
-            print >>out, word
-    finally:
-        out.close()
-
-def show_category_terms(category_terms, filename):
-
-    """
-    Show the 'category_terms' mapping in 'filename', with each correspondence in
-    the mapping being formatted as the category followed by each distinct term
-    associated with the category.
-    """
-
-    l = category_terms.items()
-    l.sort()
-    out = codecs.open(filename, "w", encoding="utf-8")
-    try:
-        for category, terms in l:
-            terms = list(set(terms))
-            terms.sort()
-            print >>out, category
-            for term in terms:
-                print >>out, unicode(term)
-            print >>out
-    finally:
-        out.close()
-
-def show_common_terms(common_terms, filename):
-
-    """
-    Show 'common_terms' in 'filename', this illustrating each term together with
-    the entities (categories or fragments) in which it appears.
-    """
-
-    # Sort the terms and entities by increasing number of entities.
-
-    l = common_terms.items()
-    l.sort(cmp=cmp_value_lengths)
-
-    out = codecs.open(filename, "w", encoding="utf-8")
-    try:
-        for term, entities in l:
-            print >>out, unicode(term), ",".join(map(lambda e: e.label(), entities))
-    finally:
-        out.close()
-
-def show_connections(connections, filename):
-
-    "Write a report of 'connections' to 'filename'."
-
-    connections.sort(key=lambda x: x.measure())
-    out = codecs.open(filename, "w", encoding="utf-8")
-    try:
-        for connection in connections:
-            for term, weight in connection.similarity.items():
-                print >>out, unicode(term), weight,
-            print >>out
-            for fragment in connection.fragments:
-                print >>out, fragment.text
-            print >>out
-    finally:
-        out.close()
-
-def show_fragments(fragments, filename):
-
-    "Write the textual representation of 'fragments' to 'filename'."
-
-    out = codecs.open(filename, "w", encoding="utf-8")
-    try:
-        for fragment in fragments:
-            print >>out, fragment
-    finally:
-        out.close()
-
-def show_frequencies(frequencies, filename):
-
-    "Write the mapping of term 'frequencies' to 'filename'."
-
-    l = frequencies.items()
-    l.sort(cmp=cmp_values)
-    out = codecs.open(filename, "w", encoding="utf-8")
-    try:
-        for term, occurrences in l:
-            print >>out, unicode(term), occurrences
-    finally:
-        out.close()
-
-def show_related_fragments(related, filename, shown_relations=5):
-
-    """
-    Show the 'related' fragments: for each fragment, show the related fragments
-    via the connections, writing the results to 'filename'.
-    """
-
-    out = codecs.open(filename, "w", encoding="utf-8")
-    try:
-        for fragment, relations in related.items():
-
-            # Show the related fragments in descending order of similarity.
-
-            relations.sort(reverse=True)
-
-            # Show the principal fragment details.
-
-            print >>out, "  Id:", fragment.source, fragment.source.start, fragment.source.end
-            print >>out, "Text:", fragment.text
-            print >>out
-
-            # For each related fragment, show details including the similarity
-            # information.
-
-            for measure, relation, similarity in relations[:shown_relations]:
-
-                print >>out, "  Id:", relation.source, relation.source.start, relation.source.end
-                print >>out, " Sim: %.2f" % measure,
-
-                for term, score in similarity.items():
-                    print >>out, "%s (%.2f)" % (unicode(term), score),
-
-                print >>out
-                print >>out, "Text:", relation.text
-                print >>out
-
-            if len(relations) > shown_relations:
-                print >>out, "%d related fragments not shown." % (len(relations) - shown_relations)
-
-            print >>out, "----"
-            print >>out
-    finally:
-        out.close()
-
-def ensure_directory(name):
-    if not isdir(name):
-        mkdir(name)
 
 helptext = """\
 Need an output directory name plus a collection of text and tiers filenames for
@@ -251,19 +96,8 @@ if __name__ == "__main__":
 
     # Derive filenames for output files.
 
-    ensure_directory(outdir)
-
-    fragmentsfn = join(outdir, "fragments.txt")
-    connectionsfn = join(outdir, "connections.txt")
-    wordsfn = join(outdir, "words.txt")
-    termsfn = join(outdir, "terms.txt")
-    ctermsfn = join(outdir, "term_categories.txt")
-    ftermsfn = join(outdir, "term_fragments.txt")
-    termfreqfn = join(outdir, "term_frequencies.txt")
-    termdocfreqfn = join(outdir, "term_doc_frequencies.txt")
-    terminvdocfreqfn = join(outdir, "term_inv_doc_frequencies.txt")
-    relationsfn = join(outdir, "relations.txt")
-    dotfn = join(outdir, "graph.dot")
+    out = outputs.Output(outdir)
+    outfile = out.filename
 
     # For each fragment defined by the tiers, collect corresponding words, producing
     # fragment objects.
@@ -290,7 +124,7 @@ if __name__ == "__main__":
     # Output words.
 
     all_words = get_all_words(fragments)
-    show_all_words(all_words, wordsfn)
+    outputs.show_all_words(all_words, outfile("words.txt"))
 
     # Tidy up the data.
 
@@ -298,46 +132,49 @@ if __name__ == "__main__":
     commit_text(fragments)
 
     # Perform some processes on the words:
-    # Filtering of stop words.
-    # Selection of dictionary words.
-    # Part-of-speech tagging to select certain types of words (nouns and verbs).
-    # Normalisation involving stemming, synonyms and semantic equivalences.
+
+    # Part-of-speech tagging.
+    # Normalisation involving stemming and lower-casing of words.
 
     process_fragment_tokens(fragments, [stem_word, lower_word])
+
+    # Grouping of words into terms.
+    # Filtering of stop words by selecting certain kinds of words (nouns, verbs,
+    # adjectives).
 
     process_fragments(fragments, [group_words, filter_terms_by_pos])
 
     # Emit the fragments for inspection.
 
-    show_fragments(fragments, fragmentsfn)
+    outputs.show_fragments(fragments, outfile("fragments.txt"))
 
     # Get terms used by each category for inspection.
 
     category_terms = get_category_terms(fragments)
-    show_category_terms(category_terms, termsfn)
+    outputs.show_category_terms(category_terms, outfile("terms.txt"))
 
     # Get common terms (common between categories).
 
     common_category_terms = get_common_terms(category_terms)
-    show_common_terms(common_category_terms, ctermsfn)
+    outputs.show_common_terms(common_category_terms, outfile("term_categories.txt"))
 
     # Get common terms (common between fragments).
 
     fragment_terms = get_fragment_terms(fragments)
 
     common_fragment_terms = get_common_terms(fragment_terms)
-    show_common_terms(common_fragment_terms, ftermsfn)
+    outputs.show_common_terms(common_fragment_terms, outfile("term_fragments.txt"))
 
     # Get term/word frequencies.
 
     frequencies = word_frequencies(fragments)
-    show_frequencies(frequencies, termfreqfn)
+    outputs.show_frequencies(frequencies, outfile("term_frequencies.txt"))
 
     doc_frequencies = word_document_frequencies(fragments)
-    show_frequencies(doc_frequencies, termdocfreqfn)
+    outputs.show_frequencies(doc_frequencies, outfile("term_doc_frequencies.txt"))
 
     inv_doc_frequencies = inverse_document_frequencies(doc_frequencies, len(fragments))
-    show_frequencies(inv_doc_frequencies, terminvdocfreqfn)
+    outputs.show_frequencies(inv_doc_frequencies, outfile("term_inv_doc_frequencies.txt"))
 
     # Determine fragment similarity by taking the processed words and comparing
     # fragments.
@@ -346,14 +183,14 @@ if __name__ == "__main__":
 
     # Emit the connections for inspection.
 
-    show_connections(connections, connectionsfn)
+    outputs.show_connections(connections, outfile("connections.txt"))
 
     related = get_related_fragments(connections)
-    show_related_fragments(related, relationsfn)
+    outputs.show_related_fragments(related, outfile("relations.txt"))
 
     # Produce a graph where each fragment is a node and the similarity (where
     # non-zero) is an edge linking the fragments.
 
-    write_graph(fragments, connections, dotfn)
+    graph.write_graph(fragments, connections, outfile("graph.dot"))
 
 # vim: tabstop=4 expandtab shiftwidth=4
---
+++ outputs.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+
+"""
+Output production.
+"""
+
+from utils import cmp_value_lengths, cmp_values
+
+from os import mkdir
+from os.path import isdir, join
+import codecs
+
+# Output conversion.
+
+def show_all_words(words, filename):
+
+    "Show 'words' in 'filename'."
+
+    out = codecs.open(filename, "w", encoding="utf-8")
+    try:
+        for word in words:
+            print >>out, word
+    finally:
+        out.close()
+
+def show_category_terms(category_terms, filename):
+
+    """
+    Show the 'category_terms' mapping in 'filename', with each correspondence in
+    the mapping being formatted as the category followed by each distinct term
+    associated with the category.
+    """
+
+    l = category_terms.items()
+    l.sort()
+    out = codecs.open(filename, "w", encoding="utf-8")
+    try:
+        for category, terms in l:
+            terms = list(set(terms))
+            terms.sort()
+            print >>out, category
+            for term in terms:
+                print >>out, unicode(term)
+            print >>out
+    finally:
+        out.close()
+
+def show_common_terms(common_terms, filename):
+
+    """
+    Show 'common_terms' in 'filename', this illustrating each term together with
+    the entities (categories or fragments) in which it appears.
+    """
+
+    # Sort the terms and entities by increasing number of entities.
+
+    l = common_terms.items()
+    l.sort(cmp=cmp_value_lengths)
+
+    out = codecs.open(filename, "w", encoding="utf-8")
+    try:
+        for term, entities in l:
+            print >>out, unicode(term), ",".join(map(lambda e: e.label(), entities))
+    finally:
+        out.close()
+
+def show_connections(connections, filename):
+
+    "Write a report of 'connections' to 'filename'."
+
+    connections.sort(key=lambda x: x.measure())
+    out = codecs.open(filename, "w", encoding="utf-8")
+    try:
+        for connection in connections:
+            for term, weight in connection.similarity.items():
+                print >>out, unicode(term), weight,
+            print >>out
+            for fragment in connection.fragments:
+                print >>out, fragment.text
+            print >>out
+    finally:
+        out.close()
+
+def show_fragments(fragments, filename):
+
+    "Write the textual representation of 'fragments' to 'filename'."
+
+    out = codecs.open(filename, "w", encoding="utf-8")
+    try:
+        for fragment in fragments:
+            print >>out, fragment
+    finally:
+        out.close()
+
+def show_frequencies(frequencies, filename):
+
+    "Write the mapping of term 'frequencies' to 'filename'."
+
+    l = frequencies.items()
+    l.sort(cmp=cmp_values)
+    out = codecs.open(filename, "w", encoding="utf-8")
+    try:
+        for term, occurrences in l:
+            print >>out, unicode(term), occurrences
+    finally:
+        out.close()
+
+def show_related_fragments(related, filename, shown_relations=5):
+
+    """
+    Show the 'related' fragments: for each fragment, show the related fragments
+    via the connections, writing the results to 'filename'.
+    """
+
+    out = codecs.open(filename, "w", encoding="utf-8")
+    try:
+        for fragment, relations in related.items():
+
+            # Show the related fragments in descending order of similarity.
+
+            relations.sort(reverse=True)
+
+            # Show the principal fragment details.
+
+            print >>out, "  Id:", fragment.source, fragment.source.start, fragment.source.end
+            print >>out, "Text:", fragment.text
+            print >>out
+
+            # For each related fragment, show details including the similarity
+            # information.
+
+            for measure, relation, similarity in relations[:shown_relations]:
+
+                print >>out, "  Id:", relation.source, relation.source.start, relation.source.end
+                print >>out, " Sim: %.2f" % measure,
+
+                for term, score in similarity.items():
+                    print >>out, "%s (%.2f)" % (unicode(term), score),
+
+                print >>out
+                print >>out, "Text:", relation.text
+                print >>out
+
+            if len(relations) > shown_relations:
+                print >>out, "%d related fragments not shown." % (len(relations) - shown_relations)
+
+            print >>out, "----"
+            print >>out
+    finally:
+        out.close()
+
+# Output file handling.
+
+class Output:
+
+    "A container for output files."
+
+    def __init__(self, outdir):
+        self.outdir = outdir
+        ensure_directory(self.outdir)
+
+    def filename(self, name):
+        return join(self.outdir, name)
+
+def ensure_directory(name):
+    if not isdir(name):
+        mkdir(name)
+
+# vim: tabstop=4 expandtab shiftwidth=4
