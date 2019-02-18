4d7434c3ed1e93691a5266177aa707eb85cd0e32
Paul Boddie <paul.boddie@imv.uio.no>
2019-02-19 00:07:35 +0100

    Moved original input data processing into its own function.

--- fragment.py
+++ fragment.py
@@ -50,6 +50,107 @@ from wordlist import get_wordlist_from_file
 
 
 
+def process_input_data(filenames, outfile):
+
+    "Process data from 'filenames', using 'outfile' to create output filenames."
+
+    fragments = get_fragments_from_files(filenames)
+
+    # Discard empty fragments.
+
+    fragments = filter(None, fragments)
+
+    # Discard uncategorised fragments.
+
+    if not all_fragments:
+        fragments = filter(lambda f: f.category and f.category.complete(), fragments)
+
+    # Fix fragment categories.
+
+    if category_map:
+        fix_category_names(fragments, category_map)
+
+    # Obtain the raw input words.
+
+    all_words = get_all_words(fragments)
+
+    # Tidy up the data.
+
+    process_fragments(fragments, [normalise_accents])
+    commit_text(fragments)
+
+    process_fragments(fragments, [remove_punctuation_from_words])
+
+    # Perform some processes on the words:
+
+    # Part-of-speech tagging.
+    # Normalisation involving stemming and lower-casing of words.
+
+    process_fragment_tokens(fragments, [stem_word, lower_word])
+
+    # Grouping of words into terms.
+    # Filtering of stop words by selecting certain kinds of words (for example,
+    # nouns, verbs, adjectives).
+
+    process_fragments(fragments, [group_words, posfilter.filter_words])
+
+    # Selection of desired words.
+
+    if wordlist:
+        process_fragments(fragments, [wordlist.filter_words])
+
+    # Get terms used by each category for inspection.
+
+    category_terms = get_fragment_terms(fragments, lambda fragment:
+                                                   fragment.category.parent)
+
+    # Get common terms (common between categories).
+
+    common_category_terms = get_common_terms(category_terms)
+
+    # Get common terms (common between fragments).
+
+    fragment_terms = get_fragment_terms(fragments)
+    common_fragment_terms = get_common_terms(fragment_terms)
+
+    # Get term/word frequencies.
+
+    frequencies = word_frequencies(fragments)
+    doc_frequencies = word_document_frequencies(fragments)
+    inv_doc_frequencies = inverse_document_frequencies(doc_frequencies, len(fragments))
+
+    # Determine fragment similarity by taking the processed words and comparing
+    # fragments.
+
+    connections = compare_fragments(fragments, idf=inv_doc_frequencies,
+                                    terms_to_fragments=common_fragment_terms)
+
+    # Emit the fragments for inspection and potential recovery.
+
+    outputs.show_fragments(fragments, outfile("fragments.txt"))
+
+    # Emit the connection details for potential recovery.
+
+    outputs.show_connections(connections, outfile("connections.txt"), brief=True)
+
+    # Emit term details for inspection.
+
+    outputs.show_all_words(all_words, outfile("words.txt"))
+    outputs.show_category_terms(category_terms, outfile("terms.txt"))
+    outputs.show_common_terms(common_category_terms, outfile("term_categories.txt"))
+    outputs.show_common_terms(common_fragment_terms, outfile("term_fragments.txt"))
+    outputs.show_frequencies(frequencies, outfile("term_frequencies.txt"))
+    outputs.show_frequencies(doc_frequencies, outfile("term_doc_frequencies.txt"))
+    outputs.show_frequencies(inv_doc_frequencies, outfile("term_inv_doc_frequencies.txt"))
+
+    # Emit the connections for inspection.
+
+    outputs.show_connections(connections, outfile("connections_verbose.txt"))
+
+    return connections
+
+
+
 # Help text for program invocation.
 
 progname = os.path.split(sys.argv[0])[-1]
@@ -97,6 +198,8 @@ The output directory will also contain a data subdirectory containing the
 processed data in a structured form.
 """ % progname
 
+
+
 # Main program.
 
 if __name__ == "__main__":
@@ -125,110 +228,16 @@ if __name__ == "__main__":
     out = outputs.Output(outdir)
     outfile = out.filename
 
-    outfile_connections = outfile("connections.txt")
-    outfile_fragments = outfile("fragments.txt")
-
     # Either restore serialised data.
 
     if restore:
-        fragments = get_serialised_fragments(outfile_fragments)
-        connections = get_serialised_connections(outfile_connections, fragments)
+        fragments = get_serialised_fragments(outfile("fragments.txt"))
+        connections = get_serialised_connections(outfile("connections.txt"), fragments)
 
     # Or process input data.
 
     else:
-        fragments = get_fragments_from_files(filenames)
-
-        # Discard empty fragments.
-
-        fragments = filter(None, fragments)
-
-        # Discard uncategorised fragments.
-
-        if not all_fragments:
-            fragments = filter(lambda f: f.category and f.category.complete(), fragments)
-
-        # Fix fragment categories.
-
-        if category_map:
-            fix_category_names(fragments, category_map)
-
-        # Obtain the raw input words.
-
-        all_words = get_all_words(fragments)
-
-        # Tidy up the data.
-
-        process_fragments(fragments, [normalise_accents])
-        commit_text(fragments)
-
-        process_fragments(fragments, [remove_punctuation_from_words])
-
-        # Perform some processes on the words:
-
-        # Part-of-speech tagging.
-        # Normalisation involving stemming and lower-casing of words.
-
-        process_fragment_tokens(fragments, [stem_word, lower_word])
-
-        # Grouping of words into terms.
-        # Filtering of stop words by selecting certain kinds of words (for example,
-        # nouns, verbs, adjectives).
-
-        process_fragments(fragments, [group_words, posfilter.filter_words])
-
-        # Selection of desired words.
-
-        if wordlist:
-            process_fragments(fragments, [wordlist.filter_words])
-
-        # Get terms used by each category for inspection.
-
-        category_terms = get_fragment_terms(fragments, lambda fragment:
-                                                       fragment.category.parent)
-
-        # Get common terms (common between categories).
-
-        common_category_terms = get_common_terms(category_terms)
-
-        # Get common terms (common between fragments).
-
-        fragment_terms = get_fragment_terms(fragments)
-        common_fragment_terms = get_common_terms(fragment_terms)
-
-        # Get term/word frequencies.
-
-        frequencies = word_frequencies(fragments)
-        doc_frequencies = word_document_frequencies(fragments)
-        inv_doc_frequencies = inverse_document_frequencies(doc_frequencies, len(fragments))
-
-        # Determine fragment similarity by taking the processed words and comparing
-        # fragments.
-
-        connections = compare_fragments(fragments, idf=inv_doc_frequencies,
-                                        terms_to_fragments=common_fragment_terms)
-
-        # Emit the fragments for inspection and potential recovery.
-
-        outputs.show_fragments(fragments, outfile_fragments)
-
-        # Emit the connection details for potential recovery.
-
-        outputs.show_connections(connections, outfile_connections, brief=True)
-
-        # Emit term details for inspection.
-
-        outputs.show_all_words(all_words, outfile("words.txt"))
-        outputs.show_category_terms(category_terms, outfile("terms.txt"))
-        outputs.show_common_terms(common_category_terms, outfile("term_categories.txt"))
-        outputs.show_common_terms(common_fragment_terms, outfile("term_fragments.txt"))
-        outputs.show_frequencies(frequencies, outfile("term_frequencies.txt"))
-        outputs.show_frequencies(doc_frequencies, outfile("term_doc_frequencies.txt"))
-        outputs.show_frequencies(inv_doc_frequencies, outfile("term_inv_doc_frequencies.txt"))
-
-        # Emit the connections for inspection.
-
-        outputs.show_connections(connections, outfile("connections_verbose.txt"))
+        connections = process_input_data(filenames, outfile)
 
     # Process data using the connections as input.
 
