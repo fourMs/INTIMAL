152aec49e1198251a52161fc47f66fb2ca82f3b8
Paul Boddie <paul.boddie@imv.uio.no>
2019-02-28 17:30:41 +0100

    Added scripts for inspecting fragment connectivity and similarity measures.

---
+++ scripts/connectivity.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Show fragments with a number of relations other than that expected.
+
+OUTPUT=${1:-OUTPUT}
+KIND=${2:-translation}
+NUM=${3:-3}
+
+for FN in "$OUTPUT"/data/*/"$KIND" ; do
+    if [ `ls -1 "$FN" | wc -l` != "$NUM" ] ; then
+        DN=`dirname "$FN"`
+        echo `basename "$DN"`
+    fi
+done
